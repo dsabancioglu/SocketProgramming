b88d41b031a069d4de1b221f481b055acea62e12
dilara <dilarasabancioglu@hotmail.com>
2022-03-03 17:48:47 +0300

    window places changed

--- src/widgets/server_widgets.py
+++ src/widgets/server_widgets.py
@@ -11,7 +11,7 @@ class ServerWidgets:
     def __init__(self,server):   
         print(  "serverWidgets init")
         self.server_frame = Toplevel()
-        self.server_frame.geometry('500x500+50+300')
+        self.server_frame.geometry('500x500+450+300')
         self.server_frame.title("Server")
         self.server_frame.columnconfigure(0, weight=1)
         self.server_frame.columnconfigure(1, weight=1)
